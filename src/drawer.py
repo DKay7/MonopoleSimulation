f307bd9a3a937491628f7a24f2d745b3bca34bd9
Daniil <kalinin.de@phystech.edu>
2024-01-19 20:05:33 +0300

    Refactor code, add classes. Make use of numpy vector calculations

--- src/drawer.py
+++ src/drawer.py
@@ -1,29 +1,26 @@
-from simulation import compute_position, spherical_coordinates_to_decart
+from simulation import ElectricCharge, Simulation, SphereCoordinates
 
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import numpy as np
 
-
-def start_simulation(total_time=100000):
-    points = np.empty([0, 3])
-    init_coords=(1, np.pi / 3, np.pi / 2)
-    speeds=(1 , 1 / 25 , 0)
-    speed_magnitude = np.linalg.norm(speeds)
-
-    for time in np.arange(0, total_time, 10):
-        coords = compute_position(time)
-
-        decart_coords = np.array(spherical_coordinates_to_decart(*coords))
-
-        points = np.append(points, [decart_coords], axis=0)
+def draw_simulation(total_time=100000):
+    init_coords=SphereCoordinates(1, 0, np.pi / 2)
+    init_speeds=SphereCoordinates(1, 1 / 25, 0)
+    mass = 1
+    q = 1*10**-5
+    
+    charge = ElectricCharge(init_coords, init_speeds, mass, q)
+    simulation = Simulation(charge, total_time)
+    points = simulation.run_experiment()
     
     ax = plt.axes(projection='3d')
-    x, y, z = points[:, 0], points[:, 1], points[:, 2]
-    ax.plot3D(x, y, z)
+    x, y, z = points[:, 0], points[:, 1], points[:, 2] 
+    ax.plot3D(x, y, z, '--', color='black')
     ax.scatter3D(x[0], y[0], z[0], '*', color='orange', alpha=0.5)
+    ax.scatter3D(0, 0, 0, 'A', color='green', alpha=0.9)
     
     plt.show()
 
 if __name__ == "__main__":
-    start_simulation()
+    draw_simulation()
--- src/simulation.py
+++ src/simulation.py
@@ -1,77 +1,74 @@
 import numpy as np
 from math import sqrt
 
-# def collect_points(time):
-#     points = np.empty([0, 3])
+
+class SphereCoordinates:
+    def __init__(self, r, phi, theta):
+        self.r = r
+        self.phi = phi
+        self.theta = theta
+        self.coords = np.array([r, phi, theta])
     
-#     coords=(1, 1, np.pi / 2)
-#     speeds=(1, 0 , 1)
+    @property
+    def get_coords(self):
+        return self.coords 
 
-#     for time in range(total_time):
-#         tau, sphere_coords, speeds = compute_position(time, sphere_coords, speeds)
+    @property
+    def decart(self):
+        x = self.r * np.sin(self.theta) * np.cos(self.phi)
+        y = self.r * np.sin(self.theta) * np.sin(self.phi)
+        z = self.r * np.cos(self.theta)
+
+        return np.array([x, y, z])
+
+
+class ElectricCharge:
+    def __init__(self, init_coords: SphereCoordinates=SphereCoordinates(1, 0, np.pi / 2),
+                       init_speeds: SphereCoordinates=SphereCoordinates(1, 1 / 25, 0), 
+                       mass=1, q=10**-5):
+        self.init_coords = init_coords
+        self.init_speeds = init_speeds
+        self.mass = mass
+        self.q = q
     
-#         decart_coords = np.array(spherical_coordinates_to_decart(*sphere_coords))
+    def get_params(self):
+        return (self.init_coords, self.init_speeds, self.mass, self.q)
 
-#         points = np.append(points, [decart_coords], axis=0)
+
+class Simulation:
+    def __init__(self, charge: ElectricCharge, total_time: int, time_step: int = 10):
+        self.charge = charge
+        self.total_time = total_time
+        self.time_step = time_step
     
-def compute_position(time, init_coords=(1, 0, np.pi / 2), init_speeds=(1, 1 / 25, 0), mass=1, q=10**-5):
-    # space = (t, r, theta, phi)
-    
-    k = 9*10**9
-    r0 = init_coords[0]
-    v_r = init_speeds[0]
-    v_phi = init_speeds[1] * r0
-    m = mass
+    def run_experiment(self):
+        points = np.empty((0, 3))
 
-    compute_tau = lambda t: t / sqrt(1 + v_phi**2 + v_r**2)
-    tau = compute_tau(time)
+        time = np.arange(0, self.total_time, self.time_step)
+        coords = self._compute_position(time)
+        return coords.decart.T
 
-    compute_r = lambda tau_: sqrt( (v_phi**2 + v_r**2)*tau_**2 - 2*r0*v_r*tau_ + r0**2 )
-    r = compute_r(tau)
+    def _compute_position(self, current_time):
+        # space = (t, r, phi, theta)
+        # some *magic* of ~physics~
 
-    L0 = m * r0 * v_phi
-    l = sqrt(k**2 * q**2 + L0**2)
+        k = 9*10**9
+        r0 = self.charge.init_coords.r
+        v_r = self.charge.init_speeds.r
+        v_phi = self.charge.init_speeds.phi * r0
+        m = self.charge.mass
+        q = self.charge.q
 
-    compute_alpha = lambda tau_: l/L0 * (np.arctan( ((v_phi**2 + v_r**2)*tau_ + r0*v_r)/(r0*v_phi) ) - np.arctan( v_r / v_phi ))
-    alpha = compute_alpha(tau)
-    compute_phi = lambda tau_: np.arctan( -( L0*l*np.sin(alpha) ) / ( L0**2*np.cos(alpha) + k**2 * q**2 ) )
-    phi = compute_phi(tau)
+        tau = current_time / np.sqrt(1 + v_phi**2 + v_r**2)
+        r = np.sqrt( (v_phi**2 + v_r**2)*tau**2 - 2*r0*v_r*tau + r0**2 )
 
-    theta = np.arccos(-k*q *L0/l * (1 - np.cos(alpha)) )
-    
-    # theta = np.pi / 2 + np.arctan( ( l**2 - 2*L0**2 )  / ( 2*k*q*L0 ) )
-    # theta = np.pi / 2 - np.pi / 100
+        L0 = m * r0 * v_phi
+        l = np.sqrt(k**2 * q**2 + L0**2)
 
-    return (r, phi, theta)
+        alpha = l/L0 * (np.arctan( ((v_phi**2 + v_r**2)*tau + r0*v_r)/(r0*v_phi) ) - np.arctan( v_r / v_phi ))
+        phi = np.arctan( -( L0*l*np.sin(alpha) ) / ( L0**2*np.cos(alpha) + k**2 * q**2 ) )
+        theta = np.arccos(-k*q *L0/l * (1 - np.cos(alpha)) )
+        
+        return SphereCoordinates(r, phi, theta)
 
-# def compute_position(time, init_coords=(10, 0, np.pi / 7), init_speeds=(-1, -1, 1), mass=1, q=10**-7):
-#     time0 = 0
-#     r0 = init_coords[0]
-#     theta0 = init_coords[1]
-#     phi0 = init_coords[2]
-#     speed = np.linalg.norm(np.array(init_speeds))
-
-#     m = mass
-#     light_speed = 3*10**8
-
-#     r = np.sqrt(r0**2 + speed**2 * (time - time0)**2)
-    
-#     theta = theta0 - np.pi / 100
-#     # theta = theta0 + np.arctan(m*r0*)
-    
-#     psi = np.arctan(speed * (time - time0) / r0)
-#     phi = psi / np.sin(theta) + phi0
-
-
-#     return (r, theta, phi)
-
-def derivative(function, point, h=1e-6):
-    return (function(point + h) - function(point - h)) / (2 * h)
-
-def spherical_coordinates_to_decart(r, phi, theta):
-    x = r * np.sin(theta) * np.cos(phi)
-    y = r * np.sin(theta) * np.sin(phi)
-    z = r * np.cos(theta)
-
-    return (x, y, z)
  
